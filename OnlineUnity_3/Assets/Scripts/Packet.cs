b4ff0a18151982a8303a90642ebb70b245e19d0a
GrandNewStart <hbjw123@naver.com>
2020-01-08 21:11:22 +0900

    Started 3rd tutorial project(network programming basics)

---
+++ OnlineUnity_3/Assets/Scripts/Packet.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+[Serializable]
+
+public class Packet : MonoBehaviour
+{
+    public float mouseX = 0.0f;
+    public float mouseY = 0.0f;
+
+    public static byte[] ToByteArray(Packet packet)
+    {
+        MemoryStream stream = new MemoryStream();
+        BinaryFormatter formatter = new BinaryFormatter();
+        formatter.Serialize(stream, packet.mouseX);
+        formatter.Serialize(stream, packet.mouseY);
+
+        return stream.ToArray();
+    }
+
+    public static Packet FromByteArray(byte[] input)
+    {
+        MemoryStream stream = new MemoryStream();
+        BinaryFormatter formatter = new BinaryFormatter();
+        Packet packet = new Packet();
+        packet.mouseX = (float)formatter.Deserialize(stream);
+        packet.mouseY = (float)formatter.Deserialize(stream);
+
+        return packet;
+    }
+}
---
+++ OnlineUnity_3/Assets/Scripts/Server.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+public class Server : SingletonMonoBehavior<Server>
+{
+    private Socket serverSocket = null;
+    private ArrayList Connections = new ArrayList();
+    private ArrayList Buffer = new ArrayList();
+    private ArrayList ByteBuffers = new ArrayList();
+
+    public const int portNumber = 12345;
+
+    private void Start()
+    {
+        //Create Socket
+        Debug.Log("Server start!");
+        this.serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        IPEndPoint ipLocal = new IPEndPoint(IPAddress.Any, Server.portNumber);
+
+        //Bind
+        this.serverSocket.Bind(ipLocal);
+
+        //Listen
+        Debug.LogWarning("Start Listening...");
+        this.serverSocket.Listen(100);
+    }
+
+    void SocketCleanUp()
+    {
+        if(this.serverSocket != null)
+        {
+            this.serverSocket.Close();
+        }
+        this.serverSocket = null;
+
+        foreach(Socket client in this.Connections)
+        {
+            client.Close();
+        }
+        this.Connections.Clear();
+    }
+
+    private void OnApplicationQuit()
+    {
+        SocketCleanUp();
+    }
+
+    private void Update()
+    {
+        ArrayList listenList = new ArrayList();
+        listenList.Add(this.serverSocket);
+
+        Socket.Select(listenList, null, null, 1000);
+
+        for (int i = 0; i < listenList.Count; i++)
+        {
+            Socket newConnection = ((Socket)listenList[i]).Accept();
+            this.Connections.Add(newConnection);
+            this.ByteBuffers.Add(new ArrayList());
+            Debug.Log("New client is connected.");
+        }
+    }
+}
